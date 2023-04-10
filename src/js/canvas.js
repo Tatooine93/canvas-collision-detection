9eecc5188a260d76b78e698c720825165f7f7bcb
Tatooine93 <nowakantoine93@gmail.com>
2023-04-10 02:39:34 +0200

    first commit

---
+++ src/js/canvas.js
@@ -0,0 +1,80 @@
+import utils from './utils'
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2
+}
+
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+
+// Event Listeners
+addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX
+  mouse.y = event.clientY
+})
+
+addEventListener('resize', () => {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+
+  init()
+})
+
+function getDistance(x1, y1, x2, y2){
+  
+}
+
+// Objects
+class Circle {
+  constructor(x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
+  }
+
+  update() {
+    this.draw()
+  }
+}
+
+// Implementation
+let circle1;
+let circle2;
+function init() {
+  circle1 = new Circle(300, 300, 100, 'black');
+  circle2 = new Circle(undefined, undefined, 30, 'red');
+
+  for (let i = 0; i < 400; i++) {
+    // objects.push()
+  }
+}
+
+// Animation Loop
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
+
+  //c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+  circle1.update()
+  circle2.x = mouse.x;
+  circle2.y = mouse.y;
+  circle2.update()
+}
+
+init()
+animate()
